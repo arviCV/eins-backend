a769bfadfe3802dd8855e7748676ec4b77267f95
RAGAVI071 <148204913+RAGAVI071@users.noreply.github.com>
2025-06-12 14:39:08 +0530

    add user info

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return JSONResponse(content={"message": "Welcome to the FastAPI root endpoint!"})
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
 
-app = FastAPI()
-
-# In-memory storage for user data
 users_db = []
-
-# Pydantic model to define the structure of user information
 class User(BaseModel):
     id: int
     name: str
