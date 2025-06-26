5e0ac96be8551eea58a405b73aa1c37f0a5fdead
arvind-sharma-murugan <arvind-sharma.murugan@allianz.de>
2025-06-26 17:30:11 +0200

    Add the correct file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
-import model
+import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
-model.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 class ChoiceBase(BaseModel):
     choice_text:str
@@ -43,11 +43,11 @@ async def read_choices(question_id:int):
     
 @app.post("/questions/")
 async def create_questions(question: QuestionBase, db: db_dependency):
-    db_question = model.Questions(question_text=question.question_text)
+    db_question = models.Questions(question_text=question.question_text)
     db.add(db_question)
     db.commit()
     db.refresh(db_question) 
     for choice in question.choices:
-        db_choice = model.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
         db.add(db_choice)
     db.commit()
