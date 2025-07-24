ce02fc9ac5916656c656e7c82bfbf3da2001569c
RAGAVI071 <148204913+RAGAVI071@users.noreply.github.com>
2025-07-24 23:26:41 +0530

    add model

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class Questions(Base):
     id = Column(Integer, primary_key=True, index=True)
     question_text = Column(String, index=True)
     
-class choices(Base):
+class Choices(Base):
     __tablename__ = 'choices'
 
     id = Column(Integer, primary_key=True, index=True)
