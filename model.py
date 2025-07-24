b89e748b076fa2a8bb4f01121a15a4c808754d8e
RAGAVI071 <148204913+RAGAVI071@users.noreply.github.com>
2025-07-24 20:04:00 +0530

    add model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
+
 
 class Users(Base):
     __tablename__ = 'users'
@@ -8,3 +9,17 @@ class Users(Base):
     username = Column(String, unique=True)
     hashed_password = Column(String)
  
+
+class Questions(Base):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(String, index=True)
+    
+class choices(Base):
+    __tablename__ = 'choices'
+
+    id = Column(Integer, primary_key=True, index=True)
+    choice_text = Column(String, index=True)
+    is_correct = Column(Boolean, default=False)
+    questions_id = Column(Integer, ForeignKey("questions.id"))
