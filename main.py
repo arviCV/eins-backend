9bcd4cfed05f8aa694cb2b6b4af13d553804b85a
Arvind <arvind4nitkkr@gmail.com>
2025-07-28 17:38:44 +0200

    Implementaion of login and logout (#9)
    * add login
    
    * add login
    
    * add information
    
    * add info
    
    * Make small beautification changes
    
    * add login
    
    * add info
    
    * add info
    
    * add user login
    
    * add data
    
    * add user
    
    * add info
    
    * add model
    
    * add info
    
    * add model
    
    * add correct file
    
    ---------
    
    Co-authored-by: RAGAVI071 <148204913+RAGAVI071@users.noreply.github.com>
    Co-authored-by: arvind-sharma-murugan <arvind-sharma.murugan@allianz.de>

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
-from typing import List, Annotated
-import models
+import model
 from database import engine, SessionLocal
+from typing import List, Annotated
 from sqlalchemy.orm import Session
+import auth
+from auth import get_current_user
+
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+app.include_router(auth.router)
 
-class ChoiceBase(BaseModel):
-    choice_text:str
-    is_correct:bool
-
-class QuestionBase(BaseModel):
-    question_text:str
-    choices:List[ChoiceBase]
+model.Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -25,29 +22,46 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
-@app.get("/questions/{question_id}")
-async def read_question(question_id: int, db: db_dependency):
-    result =db.query(models.Questions).filter(models.Questions.id == question_id).first()
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return {"User": user}
+
+
+class ChoiceBase(BaseModel):
+    choice_text:str
+    is_correct:bool
+
+class QuestionBase(BaseModel):
+    question_text:str
+    choices:List[ChoiceBase]
+
+
+@app.get("/questions/{questions_id}")
+async def read_question(questions_id: int, db: db_dependency, user: user_dependency):
+    result =db.query(model.Questions).filter(model.Questions.id == questions_id).first()
     if not result:
         raise HTTPException(status_code=404, detail='Question is not found')
     return result
 
-@app.get("/choices/{question_id}")
-async def read_choices(question_id:int, db:db_dependency):
-    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
+@app.get("/choices/{questions_id}")
+async def read_choices(questions_id:int, db: db_dependency, user: user_dependency):
+    result = db.query(model.Choices).filter(model.Choices.questions_id == questions_id).all()
     if not result:
-        raise HTTPException(status_code=404, detail='Choices is not found')
+        raise HTTPException(status_code=404, detail='choices is not found')
     return result
     
 @app.post("/questions/")
-async def create_questions(question: QuestionBase, db: db_dependency):
-    db_question = models.Questions(question_text=question.question_text)
+async def create_questions(question: QuestionBase, db: db_dependency, user: user_dependency):
+    db_question = model.Questions(question_text=question.question_text)
     db.add(db_question)
     db.commit()
     db.refresh(db_question) 
     for choice in question.choices:
-        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db_choice = model.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, questions_id=db_question.id)
         db.add(db_choice)
     db.commit()
--- model.py
+++ model.py
@@ -1,6 +1,15 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+ 
+
 class Questions(Base):
     __tablename__ = 'questions'
 
@@ -13,4 +22,4 @@ class Choices(Base):
     id = Column(Integer, primary_key=True, index=True)
     choice_text = Column(String, index=True)
     is_correct = Column(Boolean, default=False)
-    question_id = Column(Integer, ForeignKey("questions.id"))
+    questions_id = Column(Integer, ForeignKey("questions.id"))
